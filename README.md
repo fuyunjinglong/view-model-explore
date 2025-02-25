717705bd30cea0a9eacf153663e3c58f37900225
fuyunjinglong <806435328@qq.com>
2025-02-25 22:40:25 +0800

    pixi.js

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 实现 UI 层和业务逻辑分离层架构分离，为了测试重度前端项目
 采用 Functional Core, Imperative Shell 模式实现逻辑分层
+技术栈：vue3+TS+vitest+custom render+pixi.js
+pixi.js:基于 canvas 的游戏引擎框架
+custom render：vue 提供的高阶自定义渲染 API
+
+任务
+
+1.基于 custom render api 实现把视图渲染到 canvas
+1.1 引入并 init pixi.js
+1.2 重写 renderer{({}) 渲染接口
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,64 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import './style.css'
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+import { Application, Container, Text } from "pixi.js";
+
+const app = new Application();
+await app.init({
+  width: 1700,
+  height: 650,
+});
+document.body.appendChild(app.canvas);
+
+import { createRenderer } from "vue";
+
+const render = createRenderer<Container, Container>({
+  createElement(type) {
+    // 创建容器
+    let element;
+    switch (type) {
+      case "Container":
+        element = new Container();
+        break;
+      default:
+        throw new Error(`type不存在${type}`);
+        break;
+    }
+    return element;
+  },
+  patchProp() {
+    // 更新属性
+  },
+  insert(el, parent) {
+    // 插入节点
+    if (el && parent) {
+      parent.addChild(el);
+    }
+  },
+  remove(el) {
+    // 移除节点
+    if (el && el.parent) {
+      el.parent.removeChild(el);
+    }
+  },
+  createText(text) {
+    // 创建文本
+    return new Text(text);
+  },
+  createComment(text) {
+    return new Text(text);
+  },
+  setText() {},
+  setElementText() {},
+  parentNode(node) {
+    return node.parent;
+  },
+  nextSibling() {
+    // 返回兄弟节点
+    return null;
+  },
+});
+
+render.createApp(App).mount(game.stage);
