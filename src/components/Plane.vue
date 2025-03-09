081f009fe4211dee1e524700e6ec2107de5d0855
fuyunjinglong <806435328@qq.com>
2025-03-09 15:22:33 +0800

    vitest

--- src/components/Plane.vue
+++ src/components/Plane.vue
@@ -7,7 +7,7 @@
 <script setup lang="ts">
 import { reactive, type PropType } from "vue";
 import planeImg from "../assets/img/myPlane.png";
-import { setupPlane } from "../game/Plane";
+// import { setupPlane } from "../game/Plane";
 import { Plane } from "../game";
 
 // 优化代码小技巧：飞机按下开火键需要发射子弹，所以子弹的初始化位置等与飞机有关。既然耦合，所以plane变量优化到外部
@@ -20,21 +20,24 @@ const { plane } = defineProps({
 });
 
 window.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
+  if (["Space", "Enter"].includes(e.code)) {
     plane.attack();
   }
-
   switch (e.code) {
     case "ArrowDown":
+    case "KeyS":
       plane.moveDown();
       break;
     case "ArrowUp":
+    case "KeyW":
       plane.moveUp();
       break;
     case "ArrowLeft":
+    case "KeyA":
       plane.moveLeft();
       break;
     case "ArrowRight":
+    case "KeyD":
       plane.moveRight();
       break;
     default:
